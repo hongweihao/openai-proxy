e765d16dfb6a3916d76675e296b324a00215938b
hongweihao <karlhong.weihao@qq.com>
2023-07-10 11:12:42 +0800

    feat: azure gpt proxy

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # OpenAI Proxy
 
-Super simple implementation of OpenAI API proxy forwarding based on FastAPI and httpx.
+Super simple implementation of OpenAI API and Azure GPT API proxy forwarding based on FastAPI and httpx.
+
+
 
 ## Get start
 
@@ -15,6 +17,22 @@ pip install -r requirments.txt
 ```shell
 python -m uvicorn app:app
 ```
+
+## Usage
+
+### 1. OpenAI 
+
+![openai_usage_authorization.png](images/openai_usage_authorization.png)
+
+![openai_usage_body.png](images/openai_usage_body.png)
+
+### 2. Azure GPT
+
+![azure_gpt_usage_authorization.png](images/azure_gpt_usage_authorization.png)
+
+![azure_gpt_usage_header.png](images/azure_gpt_usage_header.png)
+
+![azure_gpt_usage_body.png](images/azure_gpt_usage_body.png)
 
 # Credit
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import httpx
 from fastapi import FastAPI, HTTPException, Request, status
 from starlette.background import BackgroundTask
 from starlette.middleware.cors import CORSMiddleware
-import httpx
 from starlette.responses import StreamingResponse
 
 app = FastAPI()
@@ -16,37 +16,59 @@ app.add_middleware(
 
 
 @app.post("/openai/{path:path}")
-async def proxy(request: Request, path: str):
-    client = httpx.AsyncClient(base_url="https://api.openai.com", http1=True, http2=False)
-    url = httpx.URL(path=path, query=request.url.query.encode("utf-8"))
+async def openai_proxy(request: Request, path: str):
+    base_url = "https://api.openai.com"
+    _path = path
+    _query = request.url.query
 
     headers = dict(request.headers)
     key = headers.pop('authorization', "")
-    auth_headers_dict = {"Content-Type": "application/json", "Authorization": key}
+    _headers = {"Content-Type": "application/json", "Authorization": key}
+
+    return await proxy(request, base_url, _path, _query, _headers, 600)
+
+
+@app.post("/azure/{path:path}")
+async def azure_gpt_proxy(request: Request, path: str):
+    # resource_name = os.environ.get('AZURE_RESOURCE')
+    # deployment = os.environ.get('AZURE_DEPLOYMENT')
+    # api_version = os.environ.get('AZURE_API_VERSION')
+    # api_key = os.environ.get('AZURE_API_KEY')
+
+    headers = dict(request.headers)
+    resource_name = headers.pop('resource-name', "")
+    deployment = headers.pop('deployment-name', "")
+    api_version = headers.pop('api-version', "")
+    api_key = headers.pop('api-key', "")
+
+
+    base_url = f"https://{resource_name}.openai.azure.com"
+    _path = f"/openai/deployments/{deployment}/{path}"
+    _query = f"{request.url.query}&api-version={api_version}"
+    _headers = {"Content-Type": "application/json", "api-key": api_key}
+
+    return await proxy(request, base_url, _path, _query, _headers, 600)
+
+
+async def proxy(request, base_url, path, query, headers, timeout):
+    client = httpx.AsyncClient(base_url=base_url, http1=True, http2=False)
+    url = httpx.URL(path=path, query=query.encode("utf-8"))
     req = client.build_request(
         request.method,
         url,
-        headers=auth_headers_dict,
+        headers=headers,
         content=request.stream(),
-        timeout=600
+        timeout=timeout
     )
 
     try:
         r = await client.send(req, stream=True)
     except (httpx.ConnectError, httpx.ConnectTimeout) as e:
-        error_info = (
-            f"{type(e)}: {e} | "
-            f"Please check if host={request.client.host} can access?"
-        )
-        print(error_info)
-        raise HTTPException(
-            status_code=status.HTTP_504_GATEWAY_TIMEOUT, detail=error_info
-        )
+        print(f"{type(e)}")
+        raise HTTPException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, detail=e)
     except Exception as e:
-        print(f"{type(e)}:")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e
-        )
+        print(f"{type(e)}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e)
 
     return StreamingResponse(
         r.aiter_bytes(),
